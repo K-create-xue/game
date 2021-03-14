308c9fd99276d9d706f74ebb1804ef98a9143d25
展鹏 薛 <2546339860@qq.com>
2021-03-14 20:49:47 +0800

    a simle and short code

---
+++ lei3.6.cpp
@@ -0,0 +1,52 @@
+//#include<stdio.h>
+//#include<iostream>
+//#include<string.h>
+//using namespace std;
+//
+////class 类名{所以的成员}
+//class Student
+//{
+//public:
+//	//成员函数
+//	void SetStudentInfo(const char* name, const char* gender, int age)
+//	{
+//		strcpy_s(_name, name);
+//		strcpy_s(_gender, gender);
+//		_age = age;
+//	}
+//	void PrintStudentInfo()
+//	{
+//		cout << _name << " " << _gender << " " << _age << endl;
+//	}
+//	//成员变量/ 属性
+//	char _name[20]; 
+//	char _gender[3];
+//	int _age;
+//
+//};
+//void test()
+//{
+//	Student stu;
+//	stu.SetStudentInfo("abc", "男", 30);
+//	stu.PrintStudentInfo();
+//	cout << stu._gender << endl;
+//}
+//
+////void test()
+////{
+////	cout << typeid(nullptr).name() << endl;
+////}
+////void test()
+////{
+////	int arr[] = { 1,2,3,4,5,6,7 };
+////	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+////	{
+////		cout << arr[i] <<" ";
+////	}
+////	cout << endl;
+////}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
